87df7b9007dee120a5e7a5a930bebd3e854535c6
Antonis Papaioannou <mrantonp@gmail.com>
2024-03-10 11:27:02 +0200

    Complete billboards Api

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import { Plus } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import { BillboardColumn, columns } from './columns';
 import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -34,6 +35,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
       </div>
       <Separator />
       <DataTable searchKey='label' columns={columns} data={data}/>
+      <Heading title='API' description='API calls for billboards'/>
+      <Separator />
+      <ApiList entityName='billboards' entityIdName='billboardId'/>
     </>
   );
 };
--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   
-  publicRoutes: ['/anyone-can-visit-this-route'],
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  publicRoutes: ['/api/:path*'],
+  // ignoredRoutes: ['/no-auth-in-this-route'],
 });
  
 export const config = {
